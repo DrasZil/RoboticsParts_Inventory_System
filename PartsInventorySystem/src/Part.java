c46f6a7a51d80dca814218d90bd9cdaa91c3a4f4
OliverLedesma <draszil.1720@gmail.com>
2025-11-25 12:58:39 +0800

    -Added new parameters -better visual on ToString representation

--- PartsInventorySystem/src/Part.java
+++ PartsInventorySystem/src/Part.java
@@ -7,19 +7,29 @@ public class Part implements Serializable {
     private String partNumber;
     private int quantity;
     private int price;
+    private String currency;
+    private double originalPrice;
+    private String originalCurrency;
     private String category;
     private String description;
 
     //creating a constrctor
     //local parameters inside Part()
     //"this." Assigns parameter to the instance variables
-    public Part(String name, String partNumber, int quantity, int price,
+    public Part(String name, String partNumber, int quantity, int price, 
+                String currency, double originalPrice, String originalCurrency, 
                 String category, String description){
                     //bg fixed, fixed proper order
                     this.name = name;
                     this.partNumber = partNumber;
                     this.quantity = quantity;
+
                     this.price = price;
+                    //added currency, originalPrice, and originalCurrency parameters
+                    this.currency = currency;
+                    this.originalPrice = originalPrice;
+                    this.originalCurrency = originalCurrency;
+
                     this.category = category;
                     this.description = description;
                 }
@@ -55,6 +65,22 @@ public class Part implements Serializable {
         return this.price = price;
     }
 
+    public String getCurrency(){
+        return currency;
+    }
+
+    public void setCurrency(String currency){
+        this.currency = currency;
+    }
+
+    public double getOriginalPrice(){
+        return originalPrice;
+    }
+
+    public String getOriginalCurrency(){
+        return originalCurrency;
+    }
+
     public String getCategory(){
         return category;
     }
@@ -72,8 +98,23 @@ public class Part implements Serializable {
     @Override
     //toString for easy product display
     public String toString(){
-        return String.format("Name: %s, Part Number: %s, Quantity: %d, Price: %d, Category: %s, Description: %s",
-                            name, partNumber, quantity, price, category, description);
+        StringBuilder sb = new StringBuilder();
+
+            sb.append("\n ---------------- PART DETAILS ---------------- \n");
+            sb.append(String.format("Name        : %s\n", name));
+            sb.append(String.format("Part Number : %s\n", partNumber));
+            sb.append(String.format("Quantity    : %d\n", quantity));
+            sb.append(String.format("Price       : %s %d\n", currency, price));
+            sb.append(String.format("Category    : %s\n", category));
+            sb.append(String.format("Description : %s\n", description));
+
+            //show origial USD price only if it exists AND is USD
+            if (originalCurrency != null && originalCurrency.equals("USD")){
+                sb.append(String.format("Original    : %s %.2f\n", originalCurrency, originalPrice));
+            }
+            sb.append("------------------------------------------------\n");
+            
+        return sb.toString();
     }
 
     
